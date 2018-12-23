846551f4e5612e82e7db991e4fb29cc53dd48836
laulau <olaulau@gmail.com>
2018-12-23 20:23:47 +0100

    add issuer to SslInfo

--- php/SslInfos.class.php
+++ php/SslInfos.class.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/functions.inc.php';
 
 class SslInfos {
 	
@@ -6,11 +7,11 @@ class SslInfos {
 	private $rawInfos;
 	private $sslExpires;
 	private $error;
+	private $issuer;
 	
 	function __construct ($domain) {
 		$this->domain = $domain;
-		$cmd = "echo | openssl s_client -showcerts -servername $domain -connect $domain:443 2> /tmp/test.txt | openssl x509 -inform pem -noout -text > /tmp/test/txt";
-// 		$this->rawInfos = shell_exec ($cmd);
+		$cmd = "echo | openssl s_client -showcerts -servername $domain -connect $domain:443 2>> /tmp/test.txt | openssl x509 -inform pem -noout -text >> /tmp/test.txt && cat /tmp/test.txt && rm /tmp/test.txt";
 		$handle = popen ($cmd, "r");
 		$this->rawInfos = stream_get_contents($handle);
 		
@@ -19,6 +20,9 @@ class SslInfos {
 		
 		if (preg_match("/verify error:num=(.*):(.*)/", $this->rawInfos, $matches))
 			$this->error = $matches[2];
+		
+		if (preg_match("/Issuer: (C = ([^,]*)){0,1}(, ){0,1}(O = ([^,]*)){0,1}(, ){0,1}(CN = ([^,\n]*)){0,1}\n/", $this->rawInfos, $matches))
+		    $this->issuer = $matches[8];
 	}
 	
 	public function getSslExpires () {
@@ -29,13 +33,13 @@ class SslInfos {
 		return $this->error;
 	}
 	
+	public function getIssuer () {
+	    return $this->issuer;
+	}
 }
-
-// $domain = "";
-// $s = new SslInfos($domain);
-// echo $s->getSslExpires();
-
 
 $domain = "";
 $s = new SslInfos($domain);
-var_dump($s);
+echo $s->getSslExpires();
+echo $s->getError();
+echo $s->getIssuer();
